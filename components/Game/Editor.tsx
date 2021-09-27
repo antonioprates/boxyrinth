c016bd7298be53df881ad82881176ae4dbe4fe45
Antonio Prates <antonioprates@gmail.com>
2021-09-27 22:22:04 +0800

    Revert to deprecated working Clipboard
    Also bump version for publishing

--- components/Game/Editor.tsx
+++ components/Game/Editor.tsx
@@ -5,8 +5,8 @@
  *****************************************************************************/
 
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import Clipboard from '@react-native-community/clipboard'
+import { View, Text, Clipboard } from 'react-native'
+// import Clipboard from '@react-native-community/clipboard'
 import {
   GridBoard,
   MazeSquare,
